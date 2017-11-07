439455c4cf00ae20d5f86eed91c8c42f9c85e4ae
Rahul Vailapully <rahul.vailapully@gmail.com>
2017-11-07 15:43:57 +0530

    changes

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -2,8 +2,17 @@ package io.pivotal.pal.tracker.allocations;
 
 import org.springframework.web.client.RestOperations;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ProjectClient {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final Map<Long, ProjectInfo> projectsCache = new ConcurrentHashMap<>();
     private final RestOperations restOperations;
     private final String registrationServerEndpoint;
 
@@ -12,7 +21,15 @@ public class ProjectClient {
         this.registrationServerEndpoint = registrationServerEndpoint;
     }
 
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
         return restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
     }
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+        logger.info("Getting project with id {} from cache", projectId);
+        return projectsCache.get(projectId);
+    }
+
+
 }
